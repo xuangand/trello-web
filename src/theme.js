ea60f4342e611e3edec535e6c355b7ad061d52f5
Dang Pham <xuandang854@gmail.com>
2023-12-12 14:06:18 +0700

    add responsive appbar

--- src/theme.js
+++ src/theme.js
@@ -25,6 +25,25 @@ const theme = extendTheme({
 
   components: {
     // Name of the component
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#bdc3c7',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#00b894',
+            borderRadius: '8px'
+          }
+        }
+      }
+    },
+
     MuiButton: {
       styleOverrides: {
         // Name of the slot
