24645f875a3644d476f2b76135f7a72287623a4c
xuangand <xuandang854@gmail.com>
2024-01-26 08:32:45 +0700

    add axios to call API

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,16 +1,29 @@
+import { useEffect, useState } from 'react'
 import { Container } from '@mui/material'
 import Appbar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
+//import { mockData } from '~/apis/mock-data'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    //Tạm thời fix cứng boardId, chúng ta sẽ sử dụng react-router-dom để lấy chuẩn boardId từ URL về
+    const boardId = '65b0807e24115741ebb37469'
+    //Call API
+    fetchBoardDetailsAPI(boardId).then(board => {
+      setBoard(board)
+    })
+  }, [])
+
   return (
     <>
       <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
         <Appbar />
-        <BoardBar board={mockData?.board} />
-        <BoardContent board={mockData?.board} />
+        <BoardBar board={board} />
+        <BoardContent board={board} />
       </Container>
     </>
   )
